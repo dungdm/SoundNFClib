851d2dbbd52c0362ef0df7e868c67897457ee33e
Manh Dung Do <dungdm@gmail.com>
2013-12-09 23:21:32 +0700

    Initial Commit

---
+++ SoundNFClib/SoundNFClib.h
@@ -0,0 +1,13 @@
+//
+//  SoundNFClib.h
+//  SoundNFClib
+//
+//  Created by Manh Dung Do on 12/9/13.
+//  Copyright (c) 2013 Moca. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SoundNFClib : NSObject
+
+@end
---
+++ SoundNFClib/SoundNFClib.m
@@ -0,0 +1,13 @@
+//
+//  SoundNFClib.m
+//  SoundNFClib
+//
+//  Created by Manh Dung Do on 12/9/13.
+//  Copyright (c) 2013 Moca. All rights reserved.
+//
+
+#import "SoundNFClib.h"
+
+@implementation SoundNFClib
+
+@end
